1922a9381f96181911a3309a957b858d4a2e46fa
Cas de Reuver <cas@reuver.co>
2020-12-09 13:21:06 +0100

    Initial

---
+++ static/main.js
@@ -0,0 +1,120 @@
+// Config variables: change them to point to your own servers
+const SIGNALING_SERVER_URL = '/';
+const TURN_SERVER_URL = 'dev4-lux1.raaw.eu:3478';
+const TURN_SERVER_USERNAME = 'casdr';
+const TURN_SERVER_CREDENTIAL = 'Oochah6sheiF';
+
+const PC_CONFIG = {
+  iceServers: [
+    {
+      urls: 'turn:' + TURN_SERVER_URL + '?transport=tcp',
+      username: TURN_SERVER_USERNAME,
+      credential: TURN_SERVER_CREDENTIAL
+    },
+    {
+      urls: 'turn:' + TURN_SERVER_URL + '?transport=udp',
+      username: TURN_SERVER_USERNAME,
+      credential: TURN_SERVER_CREDENTIAL
+    }
+  ]
+};
+
+let socket = io(SIGNALING_SERVER_URL, { autoConnect: false });
+
+socket.on('data', (data) => {
+  console.log('Data received: ',data);
+  handleSignalingData(data);
+});
+
+socket.on('ready', () => {
+  console.log('Ready');
+  createPeerConnection();
+  sendOffer();
+});
+
+let sendData = (data) => {
+  socket.emit('data', data);
+};
+
+let pc;
+let localStream;
+let remoteStreamElement = document.querySelector('#remoteStream');
+
+let getLocalStream = () => {
+  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+    .then((stream) => {
+      console.log('Stream found');
+      localStream = stream;      socket.connect();
+    })
+    .catch(error => {
+      console.error('Stream not found: ', error);
+    });
+}
+
+let createPeerConnection = () => {
+  try {
+    pc = new RTCPeerConnection(PC_CONFIG);
+    pc.onicecandidate = onIceCandidate;
+    pc.onaddstream = onAddStream;
+    pc.addStream(localStream);
+    console.log('PeerConnection created');
+  } catch (error) {
+    console.error('PeerConnection failed: ', error);
+  }
+};
+
+let sendOffer = () => {
+  console.log('Send offer');
+  pc.createOffer().then(
+    setAndSendLocalDescription,
+    (error) => { console.error('Send offer failed: ', error); }
+  );
+};
+
+let sendAnswer = () => {
+  console.log('Send answer');
+  pc.createAnswer().then(
+    setAndSendLocalDescription,
+    (error) => { console.error('Send answer failed: ', error); }
+  );
+};
+
+let setAndSendLocalDescription = (sessionDescription) => {
+  pc.setLocalDescription(sessionDescription);
+  console.log('Local description set');
+  sendData(sessionDescription);
+};
+
+let onIceCandidate = (event) => {
+  if (event.candidate) {
+    console.log('ICE candidate');
+    sendData({
+      type: 'candidate',
+      candidate: event.candidate
+    });
+  }
+};
+
+let onAddStream = (event) => {
+  console.log('Add stream');
+  remoteStreamElement.srcObject = event.stream;
+};
+
+let handleSignalingData = (data) => {
+  switch (data.type) {
+    case 'offer':
+      createPeerConnection();
+      pc.setRemoteDescription(new RTCSessionDescription(data));
+      sendAnswer();
+      break;
+    case 'answer':
+      pc.setRemoteDescription(new RTCSessionDescription(data));
+      break;
+    case 'candidate':
+      pc.addIceCandidate(new RTCIceCandidate(data.candidate));
+      break;
+  }
+};
+
+// Start connection
+getLocalStream();
